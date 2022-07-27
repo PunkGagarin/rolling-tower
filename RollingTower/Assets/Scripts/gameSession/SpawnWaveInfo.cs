946386afc22318b712218005c3404cea1b1db28a
Roman <41580710+PunkGagarin@users.noreply.github.com>
2022-07-27 22:36:38 +0300

    Base of EnemySpawner.cs, base of SpawnWaveInfo.cs, base of EnemyWave.cs, base of GameSessionManager.cs RoundStageType.cs

---
+++ RollingTower/Assets/Scripts/gameSession/SpawnWaveInfo.cs
@@ -0,0 +1,7 @@
+using System;
+
+[Serializable]
+public struct SpawnWaveInfo {
+    public EnemyUnitType _enemyUnitType;
+    public int _enemyCount;
+}
---
+++ RollingTower/Assets/Scripts/gameSession/rounds/GameSessionManager.cs
@@ -0,0 +1,55 @@
+using System;
+using UnityEngine;
+
+public class GameSessionManager : MonoBehaviour {
+
+    public Action<RoundStageType, RoundStageType> OnRoundStageOver = delegate { };
+
+    private RoundStageType _currentRoundStageType;
+
+    private bool _isGameOver;
+
+    private int _currentRoundNumber;
+
+    [SerializeField]
+    private int _maxRoundNumber = 20;
+
+    public static GameSessionManager GetInstance { get; private set; }
+
+    private void Awake() {
+        if (GetInstance == null) {
+            GetInstance = this;
+        }
+        OnRoundStageOver += OnRoundStageOverHandler;
+    }
+
+    private void Start() {
+    }
+
+    private void GoToNextStage() {
+        if (_currentRoundStageType == RoundStageType.Fighting) {
+            _currentRoundStageType = RoundStageType.CardChoosing;
+            OnRoundStageOver.Invoke(RoundStageType.Fighting, RoundStageType.CardChoosing);
+        } else if (_currentRoundStageType == RoundStageType.CardChoosing) {
+            _currentRoundStageType = RoundStageType.ResourceGathering;
+            OnRoundStageOver.Invoke(RoundStageType.CardChoosing, RoundStageType.ResourceGathering);
+            //showCardChoosingUI should be implemented in another class and be subscribed to this event
+        } else if (_currentRoundStageType == RoundStageType.ResourceGathering) {
+            _currentRoundStageType = RoundStageType.Fighting;
+            _currentRoundNumber++;
+            OnRoundStageOver.Invoke(RoundStageType.ResourceGathering, RoundStageType.Fighting);
+        } else {
+            throw new ArgumentOutOfRangeException();
+        }
+    }
+
+    private void OnRoundStageOverHandler(RoundStageType prevStageType, RoundStageType nextStageType) {
+        Debug.Log("Round stage over: " + prevStageType + " next stage: " + nextStageType);
+    }
+
+
+    private void ChangeGameState() {
+    }
+
+
+}
