da3a01d16dcbd45893e244245551b3235a22326f
Roman <41580710+PunkGagarin@users.noreply.github.com>
2022-10-21 09:34:18 +0300

    mainMenu base ui

---
+++ RollingTower/Assets/Scripts/UI/ButtonUI.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI {
+
+    public abstract class ButtonUI : MonoBehaviour {
+
+        private Button _button;
+
+        private void Awake() {
+            _button = GetComponent<Button>();
+            _button.onClick.AddListener(ButtonAction);
+        }
+
+        protected abstract void ButtonAction();
+
+    }
+
+}
