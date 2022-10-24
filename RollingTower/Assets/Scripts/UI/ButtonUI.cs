49047a4b18c3a312b8f3684d23af29d4ee3ea69a
Roman <41580710+PunkGagarin@users.noreply.github.com>
2022-10-24 09:15:21 +0300

    First Level Design

--- RollingTower/Assets/Scripts/UI/ButtonUI.cs
+++ RollingTower/Assets/Scripts/UI/ButtonUI.cs
@@ -5,10 +5,14 @@ namespace UI {
 
     public abstract class ButtonUI : MonoBehaviour {
 
+        private static string MyName = "asdf";
+
         private Button _button;
 
-        private void Awake() {
+        private void Awake() 
+        {
             _button = GetComponent<Button>();
+
             _button.onClick.AddListener(ButtonAction);
         }
 
